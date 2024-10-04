417cd8229db907879c32b1ec57db7a9bdb85a5bd
Samuel Rahsepar Mohammadi <samuel.rahseparmoha@learnet.se>
2024-10-04 15:03:44 +0200

    första version av spelet

---
+++ gladiatorspel/gladiatorn.py
@@ -0,0 +1,10 @@
+import keyboard
+
+
+
+print ("Du är en gladiator i rome och du ska förbereda dig för att slås mot en annan gladiator, du måste vinna den här fighten för att överleva och för att försäna din frihet.\n")
+
+print ("tryck på en tangent för att fortsätta.") 
+m.read_event()
+
+print ("hej")
